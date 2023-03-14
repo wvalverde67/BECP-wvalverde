7d1dcd5914631cef3f0f6cde9fe8b36bb9641804
Willie Valverde <willie_valverde@hotmail.com>
2023-03-14 17:29:01 -0600

    13 mar 2023
    13 mar 2023

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+# to register the model in admin.py
+
+from .models import Menu
+from .models import Booking
+
+
+admin.site.register(Menu)
+admin.site.register(Booking)
--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+# model for booking table
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    no_of_guest = models.IntegerField(name='no_of_guest', null=False)
+    bookingdate = models.DateField(name='bookingdate', null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    inventory = models.IntegerField(null=False)
+
+    def __str__(self):
+        return self.name
